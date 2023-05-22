3c2b12d93562251fd2d9b3357b167bb84ba0528f
Jared Hanson <jaredhanson@gmail.com>
2023-05-22 06:33:16 -0700

    Add id primary key column to DB schema.

--- db.js
+++ db.js
@@ -7,20 +7,25 @@ var db = new sqlite3.Database('var/db/todos.db');
 
 db.serialize(function() {
   db.run("CREATE TABLE IF NOT EXISTS users ( \
+    id INTEGER PRIMARY KEY, \
     username TEXT UNIQUE, \
     hashed_password BLOB, \
     salt BLOB, \
-    name TEXT \
+    name TEXT, \
+    email TEXT UNIQUE, \
+    email_verified INTEGER \
   )");
   
   db.run("CREATE TABLE IF NOT EXISTS federated_credentials ( \
+    id INTEGER PRIMARY KEY, \
     user_id INTEGER NOT NULL, \
     provider TEXT NOT NULL, \
     subject TEXT NOT NULL, \
-    PRIMARY KEY (provider, subject) \
+    UNIQUE (provider, subject) \
   )");
   
   db.run("CREATE TABLE IF NOT EXISTS todos ( \
+    id INTEGER PRIMARY KEY, \
     owner_id INTEGER NOT NULL, \
     title TEXT NOT NULL, \
     completed INTEGER \
